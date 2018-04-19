ff45e759e8f63083ad2c2ddab531cc4d7f0415d8
Leenawat Papahom <leenawat.pa@gmail.com>
2018-04-19 10:26:20 +0700

    update datebase, add config test, prod, config logback

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,5 @@ insert into APP_USER(ID, PASSWORD, USERNAME) values(1, '$2a$10$bnC26zz//2cavYoSC
 insert into APP_USER(ID, PASSWORD, USERNAME) values(2, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', 'leenawat.pa@gmail.com');
 insert into USER_ROLE(APP_USER_ID, ROLE) values(1, 'ADMIN');
 insert into USER_ROLE(APP_USER_ID, ROLE) values(1, 'PREMIUM_MEMBER');
+--insert into USER_ROLE(APP_USER_ID, ROLE) values(1, 'REFRESH_TOKEN');
 insert into USER_ROLE(APP_USER_ID, ROLE) values(2, 'PREMIUM_MEMBER');
